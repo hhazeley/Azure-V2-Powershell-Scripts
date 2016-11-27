fe3b0ede4c29f00cddb5a47a0c469b9c09ec0921
hhazeley <hhazeley@hazelnest.com>
2016-11-27 08:50:30 -0800

    CustomScriptExtenstion Scripts
    Scripts deplayed on machines using Azure CustomScriptExtension

---
+++ CustomScriptExtenstions/WinRMHTTPS-FCSC-Set.ps1
@@ -0,0 +1,15 @@
+﻿#Deploy script to machine using Custom Script Extension
+#This will created a firewall rule for 'WinRM HTTPS" and Configure WinRM for HTTPS using certificate
+Param(
+    $StorageA,
+    $StorageAAK,
+    $DNSlabelName,
+    $password,
+    $DNSLabel,
+    $thumbprint
+)
+New-NetFirewallRule -Name "WinRM HTTPS" -DisplayName "WinRM HTTPS" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 5986 -Protocol TCP
+net use X: \\$StorageA.file.core.windows.net\cert /u:$StorageA $StorageAAK
+Import-PfxCertificate -FilePath X:\$DNSlabelName.pfx -CertStoreLocation Cert:\LocalMachine\My\ -Password (ConvertTo-SecureString "$password" -AsPlainText -Force) -Exportable 
+New-WSManInstance -ResourceURI winrm/config/Listener -SelectorSet @{Address='*';Transport="HTTPS"} -ValueSet @{Hostname="$DNSLabel";CertificateThumbprint="$thumbprint"}
+net use X: /delete 
